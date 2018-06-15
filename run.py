83b741e0c44a7c0b9ad03cf75b4f06b12fe699b3
Lyz <olderlong@163.com>
2018-06-15 22:13:54 +0800

    test server

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import time
 from app.lib import *
+from app.server import *
 
 command = {
     "Type": "WVSCommand",
@@ -24,7 +25,30 @@ if __name__ == '__main__':
     time.sleep(1)
 
     common_msg.msg_server_command.data = command
-    for _ in range(10):
-        msg_bus.send_msg(common_msg.msg_server_command)
+    # for _ in range(10):
+    #     msg_bus.send_msg(common_msg.msg_server_command)
+    #
+    # time.sleep(5)
 
-    time.sleep(5)
+    server = CCServer()
+    server.start()
+    agent_state_monitor = AgentStateMonitor()
+    agent_state_monitor.start_monitor()
+
+    command = {
+        "Type": "WVSCommand",
+        "Data": {
+            "Action": "StartNewScan",
+            "Config": {  # 可选，当命令为StartNewScan时需提供该字段作为扫描参数
+                "StartURL": "http://www.cnblog.com",
+                "ScanPolicy": "Normal"
+            }
+        }
+    }
+    time.sleep(20)
+    common_msg.msg_server_command.data = command
+    # print("in run_server " + str(agent_event.event_wvs_command.dict))
+    msg_bus.send_msg(common_msg.msg_server_command)
+
+    time.sleep(50)
+    server.stop()
