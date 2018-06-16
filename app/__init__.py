e4f4f276e03b8bfd790b2a0cb98661a5b532c40e
Lyz <olderlong@163.com>
2018-06-16 13:30:33 +0800

    添加日志类对象初始化

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,24 @@
+import logging, os, time, sys
+
+def __init_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s >>> %(message)s')
+    # 文件日志
+    app_path = os.getcwd()
+    file_handler = logging.FileHandler(os.path.join(app_path, "log/{}.log".format(time.time())))
+    file_handler.setFormatter(formatter)  # 可以通过setFormatter指定输出格式
+
+    # 控制台日志
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.formatter = formatter  # 也可以直接给formatter赋值
+
+    # 为logger添加的日志处理器
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+
+__init_logger("Server")
 
 class Config:
     ServerIP = "192.168.3.2"
