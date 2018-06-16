f27af4e03c8f62bb2fdafb4fafcf309768e422c6
Lyz <olderlong@163.com>
2018-06-16 19:15:54 +0800

    修改日志文件名

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 import logging, os, time, sys
 
+
 def __init_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s >>> %(message)s')
     # 文件日志
     app_path = os.getcwd()
-    file_handler = logging.FileHandler(os.path.join(app_path, "log/{}.log".format(time.time())))
+    file_handler = logging.FileHandler(
+        os.path.join(
+            app_path,
+            "readme.md/{}.readme.md".format(
+                time.strftime("%Y_%m_%d_%H_%M_%S", time.gmtime(time.time())))))
     file_handler.setFormatter(formatter)  # 可以通过setFormatter指定输出格式
 
     # 控制台日志
@@ -16,6 +21,7 @@ def __init_logger(name):
     # 为logger添加的日志处理器
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
+
 
 
 __init_logger("Server")
