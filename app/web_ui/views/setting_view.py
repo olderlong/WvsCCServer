77775dda72ba3a05d3529c4ce5cf8ee0b97767b2
Antares <olderlong@163.com>
2018-06-21 11:34:13 +0800

    添加对配置参数的输入处理与CC服务器运行控制

--- app/web_ui/views/setting_view.py
+++ app/web_ui/views/setting_view.py
@@ -1,22 +1,18 @@
 #! /usr/bin/env python
 # _*_coding:utf-8 -*_
-
+import logging
 from flask import render_template,request,url_for,redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField, SubmitField
 from wtforms.validators import DataRequired,IPAddress,URL
 
-def singleton(cls):
-    _instance = {}
+from app.cli_ui import CliApp
+from app.web_ui.serverconfig import ServerConfig, ScanSetting
 
-    def _singleton(*args, **kargs):
-        if cls not in _instance:
-            _instance[cls] = cls(*args, **kargs)
-        return _instance[cls]
+logger = logging.getLogger("Server")
 
-    return _singleton
 
-class CCServerSetting(FlaskForm):
+class CCServerSettingForm(FlaskForm):
     CCServerIP = StringField("控制中心IP", validators=[IPAddress],default="192.168.1.31")
     CCServerPort = IntegerField("控制中心监听端口", validators=[DataRequired],default=6000)
     # Select类型，下拉单选框，choices里的内容会在Option里，里面每个项是(值，显示名)对
@@ -24,10 +20,12 @@ class CCServerSetting(FlaskForm):
         ('UDP', 'UDP'),
         ('TCP', 'TCP')
     ],default="UDP")
-    submit = SubmitField('启动控制服务器')
+
+    submit_start = SubmitField('启动控制服务器')
+    submit_stop = SubmitField('停止控制服务器')
 
 
-class ScanSetting(FlaskForm):
+class ScanSettingForm(FlaskForm):
     StartURL = StringField("起始URL", validators=[DataRequired,URL])
     # ScanPolicy = SelectField("扫描策略", validators=[DataRequired])
     # Select类型，下拉单选框，choices里的内容会在Option里，里面每个项是(值，显示名)对
@@ -39,67 +37,64 @@ class ScanSetting(FlaskForm):
     submit = SubmitField('确定')
 
 
-
-@singleton
-class ServerSetting:
-    def __init__(self, ip = "192.168.1.31", port=6000 , protocol="UDP"):
-        self.ccserver_ip = ip
-        self.ccserver_port = port
-        self.ccserver_protocol = protocol
-
-
-@singleton
-class ScanPolicy:
-    def __init__(self, url="", policy="Normal"):
-        self.start_url = url
-        self.scan_policy = policy
-
-server_set = ServerSetting()
-scan_setting = ScanPolicy()
-
 def server_setting():
-    server_setting_form = CCServerSetting()
+    server_setting_form = CCServerSettingForm()
     if server_setting_form.is_submitted():
-        server_set.ccserver_ip = server_setting_form.CCServerIP.data
-        server_set.ccserver_port = server_setting_form.CCServerPort.data
-        server_set.ccserver_protocol = server_setting_form.CCProtocol.data
-        print(server_set.ccserver_ip)
+        ccserver_ip = server_setting_form.CCServerIP.data
+        ccserver_port = server_setting_form.CCServerPort.data
+        ccserver_protocol = server_setting_form.CCProtocol.data
 
-        # return render_template("setting.html", title="配置中心", CCServerSettingForm=server_setting_form,
-        #                        ScanSettingForm=scan_setting_form)
+        logger.info("控制服务器参数：(IP:{}, Port：{}, Protocol：{})".format(
+            ccserver_ip,
+            ccserver_port,
+            ccserver_protocol
+        ))
+        cli_app = CliApp(ip=ccserver_ip, port=ccserver_port, protocol=ccserver_protocol)
+        if request.form.get("submit_start") == u"启动控制服务器":
+            cli_app.run()
+            logger.info("控制服务器已启动：(IP:{}, Port：{}, Protocol：{})".format(
+                ccserver_ip,
+                ccserver_port,
+                ccserver_protocol
+                ))
+        elif request.form.get("submit_stop") == u"停止控制服务器":
+            cli_app.stop()
+            logger.info("控制服务器已停止：(IP:{}, Port：{}, Protocol：{})".format(
+                ccserver_ip,
+                ccserver_port,
+                ccserver_protocol
+                ))
+
         return redirect(url_for("setting",title="配置中心",CCServerSettingForm=server_setting_form))
     else:
         return (url_for("setting"))
+
 
 def scan_setting():
-    server_setting_form = CCServerSetting()
-    if server_setting_form.is_submitted():
-        server_set.ccserver_ip = server_setting_form.CCServerIP.data
-        server_set.ccserver_port = server_setting_form.CCServerPort.data
-        server_set.ccserver_protocol = server_setting_form.CCProtocol.data
-        print(server_set.ccserver_ip)
-
-        # return render_template("setting.html", title="配置中心", CCServerSettingForm=server_setting_form,
-        #                        ScanSettingForm=scan_setting_form)
-        return redirect(url_for("setting",title="配置中心",CCServerSettingForm=server_setting_form))
+    scan_config = ScanSetting()
+    scan_setting_form = ScanSettingForm()
+    if scan_setting_form.is_submitted():
+        scan_config.start_url = scan_setting_form.StartURL.data
+        scan_config.scan_policy = scan_setting_form.ScanPolicy.data
+        logger.info("扫描起始URL: {}, 扫描策略: {}".format(
+            scan_config.start_url,
+            scan_config.scan_policy
+        ))
+        # return redirect(url_for("setting",title="配置中心",ScanSettingForm=scan_setting_form))
+        return redirect(url_for("monitor", title="监控中心"))
     else:
         return (url_for("setting"))
 
-def setting():
-    server_setting_form = CCServerSetting()
-    server_setting_form.CCServerIP.data = server_set.ccserver_ip
-    server_setting_form.CCServerPort.data = server_set.ccserver_port
-    server_setting_form.CCProtocol.data = server_set.ccserver_protocol
 
-    scan_setting_form = ScanSetting()
-    #
-    # if server_setting_form.is_submitted():
-    #     print(server_setting_form.CCServerIP.data)
-    #     print(server_setting_form.CCServerPort.data)
-    #     print(server_setting_form.CCProtocol.data)
-    #
-    # if scan_setting_form.is_submitted():
-    #     print(scan_setting_form.StartURL.data)
-    #     print(scan_setting_form.ScanPolicy.data)
+def setting():
+    server_setting_form = CCServerSettingForm()
+    server_setting_form.CCServerIP.data = ServerConfig.CC_SERVER_IP
+    server_setting_form.CCServerPort.data = ServerConfig.CC_SERVER_PORT
+    server_setting_form.CCProtocol.data = ServerConfig.CC_PROTOCOL
+
+    scan_config = ScanSetting()
+    scan_setting_form = ScanSettingForm()
+    scan_setting_form.StartURL.data = scan_config.start_url
+    scan_setting_form.ScanPolicy.data = scan_config.scan_policy
 
     return render_template("setting.html", title="配置中心", CCServerSettingForm=server_setting_form, ScanSettingForm=scan_setting_form)
