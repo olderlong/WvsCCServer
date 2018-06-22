772c85f87b11bf33d186bf974d09a7d30e307806
Antares <olderlong@163.com>
2018-06-22 17:26:26 +0800

    添加websocket支持

--- app/web_ui/__init__.py
+++ app/web_ui/__init__.py
@@ -2,7 +2,8 @@
 # _*_coding:utf-8 -*_
 
 from flask import Flask, render_template
-from app.web_ui.views import *
+from flask_socketio import SocketIO, emit
+
 
 
 def create_web_app():
@@ -13,12 +14,18 @@ def create_web_app():
 
 
 app = create_web_app()
+async_mode = None
+socketio = SocketIO(app, async_mode=async_mode)
 
 
-app.add_url_rule(rule="/", endpoint="home", view_func=index)
+from app.web_ui.views import *
+app.add_url_rule(rule="/", endpoint="index", view_func=index)
 app.add_url_rule(rule="/setting", endpoint="setting", view_func=setting,methods=('GET', 'POST'))
 app.add_url_rule(rule="/server_setting", endpoint="server_setting", view_func=server_setting,methods=('GET', 'POST'))
 app.add_url_rule(rule="/scan_setting", endpoint="scan_setting", view_func=scan_setting, methods=('GET', 'POST'))
 app.add_url_rule(rule="/monitor", endpoint="monitor", view_func=monitor)
 app.add_url_rule(rule="/result", endpoint="result", view_func=result)
 app.add_url_rule(rule="/readme", endpoint="readme", view_func=readme)
+
+
+__all__ = ["app", "socketio"]
