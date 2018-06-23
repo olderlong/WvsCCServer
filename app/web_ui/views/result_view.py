ab390ffbf77e38a62b08014afc115ad16dd14e45
Lyz <olderlong@163.com>
2018-06-23 21:32:35 +0800

    实现结果实时接收与更新

--- app/web_ui/views/result_view.py
+++ app/web_ui/views/result_view.py
@@ -1,8 +1,45 @@
 #! /usr/bin/env python
 # _*_coding:utf-8 -*_
-
+import logging
 from flask import render_template
+from app.web_ui import socketio
+from app.web_ui.serverconfig import ScanResult
+
+
+logger = logging.getLogger("Server")
+scan_result = ScanResult()
+wvs_result_list = scan_result.wvs_result_list
+
+
+def scan_result_handler(msg):
+    scan_res = msg.data
+    logger.info("Recive result data:\t{}".format(scan_res))
+    result = {
+        'VulType': 'ApplicationTestScriptDetected',
+        'VulUrl': 'http://192.168.3.10/zentao/theme/',
+        'VulSeverity': 'Informational',
+        'VulDetails': [
+            {
+                'url_param_variant': 'Method:  HEAD -> GET,Path:  /zentao/theme/default/zh-cn.default.css -> /zentao/theme/test.jsp',
+                'vul_reasoning': 'AppScan 请求的文件可能不是应用程序的合法部分。响应状态为“200 OK”。这表示测试成功检索了所请求的文件的内容。',
+                'CWE': None,
+                'CVE': None
+            }
+        ]
+    }
+    wvs_result_list.append(scan_res)
+
+    socketio.emit(
+        "wvs_result_update",
+        scan_res,
+        namespace="/wvs_result"
+    )
+
+
+@socketio.on("connect", namespace="/wvs_result")
+def wvs_result_connect():
+    logger.info("Wvs result websocket is connected")
 
 
 def result():
-    return render_template("result.html", title="扫描结果")
+    return render_template("result.html", title="扫描结果", scan_result_list=wvs_result_list)
