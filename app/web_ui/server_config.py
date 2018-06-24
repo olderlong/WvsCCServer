30136b3b796c30838b6349d83a21eeecd84f592d
Lyz <olderlong@163.com>
2018-06-24 12:11:22 +0800

    重新重构scan_session

--- app/web_ui/server_config.py
+++ app/web_ui/server_config.py
@@ -2,16 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-def singleton(cls):
-    _instance = {}
-
-    def _singleton(*args, **kargs):
-        if cls not in _instance:
-            _instance[cls] = cls(*args, **kargs)
-        return _instance[cls]
-
-    return _singleton
-
 
 class ServerConfig:
     SECRET_KEY = "pBw%E7feV!!tJZuP3f&QPz2%ola*2IyE"
@@ -20,18 +10,6 @@ class ServerConfig:
     CC_SERVER_IP = "192.168.3.2"
     CC_SERVER_PORT = 6000
     CC_PROTOCOL = "UDP"
-
-
-@singleton
-class ScanSetting:
-    def __init__(self, url="", policy="Normal"):
-        self.start_url = url
-        self.scan_policy = policy
-
-@singleton
-class ScanResult:
-    def __init__(self):
-        self.wvs_result_list = []
 
 
 
