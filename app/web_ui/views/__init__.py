849f0895f2ad3b9505605647e56e0d2f3d5b5165
Antares <olderlong@163.com>
2018-06-28 17:31:37 +0800

    添加任务管理视图

--- app/web_ui/views/__init__.py
+++ app/web_ui/views/__init__.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # _*_coding:utf-8 -*_
 
 from .index_view import *
@@ -6,3 +6,4 @@ from .setting_view import *
 from .monitor_view import *
 from .readme_view import *
 from .result_view import *
+from .task_view import *
