c812a619abc88d36b802521819fb9b05cd021bac
Lyz <olderlong@163.com>
2018-06-30 15:06:45 +0800

    添加报告生成、结果保存、任务保存

--- app/web_ui/views/result_view.py
+++ app/web_ui/views/result_view.py
@@ -1,13 +1,36 @@
 #! /usr/bin/env python
 # _*_coding:utf-8 -*_
-import logging
-from flask import render_template
+import logging, os
+from flask import render_template, send_from_directory, send_file
 from app.web_ui import socketio
-from app.web_ui.scan_session import ScanResult
+from app.web_ui.scan_task_manager import ScanResult,ScanSetting, ScanTaskManager
+from app.web_ui.gen_report_file import GenReport
+from app.web_ui import app
 
 
 logger = logging.getLogger("Server")
 scan_result = ScanResult()
+
+
+@app.route("/report/<timestramp>", endpoint="report", methods=['GET'])
+def report(timestramp):
+    gen_report = GenReport()
+    gen_report.gen_report()
+    return send_file(os.path.join(gen_report.report_path, gen_report.report_file_name), as_attachment=True)
+    # return send_from_directory(gen_report.report_path, gen_report.report_file_name, as_attachment=True)
+
+
+@app.route("/result/save")
+def save_result():
+    stm = ScanTaskManager()
+    logger.info("保存任务{}".format(ScanSetting().scan_name))
+    # stm.add_new_task(ScanSetting().scan_name)
+    stm.save_task(ScanSetting().scan_name)
+
+    logger.info("任务列表：{}".format(stm.scan_task_list))
+
+    return render_template("result.html", title="扫描结果", scan_result_list=scan_result.wvs_result_list)
+
 
 def scan_result_handler(msg):
     scan_res = msg.data
