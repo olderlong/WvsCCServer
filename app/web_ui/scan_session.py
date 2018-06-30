9acb9730a1f0e8ac3a2e792134c99f0e908ecdf1
Lyz <olderlong@163.com>
2018-06-30 15:03:59 +0800

    删除Scansetting类，在任务管理中重新定义

--- app/web_ui/scan_session.py
+++ app/web_ui/scan_session.py
@@ -15,39 +15,6 @@ def singleton(cls):
 
 
 @singleton
-class ScanSetting:
-    def __init__(self, url="", policy="Normal"):
-        self.start_url = url
-        self.scan_policy = policy
-
-
-@singleton
-class ScanResult:
-    def __init__(self):
-        self.wvs_result_list = []
-
-    def has(self, result):
-        identifier = "{}_{}".format(result["VulType"], result["VulUrl"])
-        for res in self.wvs_result_list:
-            res_identifier = "{}_{}".format(res["VulType"], res["VulUrl"])
-            print(res_identifier, identifier)
-            if res_identifier == identifier:
-                return True
-
-        return False
-
-    def add_scan_result(self, result):
-        existed = self.has(result)
-        if not existed:
-            self.wvs_result_list.append(result)
-        else:
-            pass
-
-    def get_scan_result(self):
-        return self.wvs_result_list
-
-
-@singleton
 class WVSState:
     def __init__(self):
         self.wvs_state_list = []
