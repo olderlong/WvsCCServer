a309fb1c999960b1d745f59557e1398a05d9ed6b
Lyz <olderlong@163.com>
2018-06-30 15:03:17 +0800

    更改启动IP

--- app/web_ui/server_config.py
+++ app/web_ui/server_config.py
@@ -8,8 +8,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class ServerConfig:
     SECRET_KEY = "pBw%E7feV!!tJZuP3f&QPz2%ola*2IyE"
     PORT = 80
-    CC_SERVER_IP = "192.168.1.31"
-    # CC_SERVER_IP = "192.168.3.2"
+    # CC_SERVER_IP = "192.168.1.31"
+    CC_SERVER_IP = "192.168.3.2"
     CC_SERVER_PORT = 6000
     CC_PROTOCOL = "UDP"
 
